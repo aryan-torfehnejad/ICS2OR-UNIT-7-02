9fe25587575de85850a9b10ce8e0e6e0e27e64e1
aryantorfehneja <aryan.torfeh-nejad@ocsbstudent.ca>
2021-03-28 19:06:27 +0000

    fixed

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.getElementById('button').addEventListener  ('click',comparison)
-let userInput=0
-function comparison(){
-document.getElementById('input').value
-userInput = parseInt(userInput)
-if(userInput < 0) {
-  document.getElementById('answer').innerHTML= "Negative"
+document.getElementById('button').addEventListener('click',comparison)
+let userInput = 0
+function comparison (){
+  document.getElementById('input').value
+  userInput = parseInt(userInput)
+  if (userInput < 0) {
+    document.getElementById('answer').innerHTML = 'Negative'
 } else {
-  document.getElementById('answer').innerHTML="Positive"
-}
+  document.getElementById('answer').innerHTML ='Positive'
+  }
 }
